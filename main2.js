ef17d1ffbf0345bcbf92d6062ad2b22f0dbac70b
yarabenabdelouaheb <yarabenabd@gmail.com>
2021-01-15 00:48:48 +0100

    add a function that check the valou of the input

--- main2.js
+++ main2.js
@@ -1,1 +1,16 @@
-alert("hello it's yara")
+alert("yara")
+
+function check(){
+    var ac=document.getElementById("un").value;
+
+    var pass=document.getElementById("psw").value
+    if(ac==="blood" && pass==="donation")
+    {
+        alert("welcom...")
+    }
+ 
+    else
+    {
+        alert("incorrect Login info.")
+    }
+}
